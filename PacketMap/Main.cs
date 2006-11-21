c711d7c9e35c2d58f98be1c65a70599bbe0cf99e
Greg Knox <knoxg@randomnoun.com>
2006-11-21 09:04:29 +0000

    Created composite flag GIF

--- PacketMap/Main.cs
+++ PacketMap/Main.cs
@@ -5,6 +5,7 @@ using System.Net;
 using Microsoft.Win32;
 using System.IO;
 using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace PacketMap
 {
@@ -32,31 +33,62 @@ namespace PacketMap
 
             Splasher.Show(typeof(SplashForm));
 
-            if (args.Length > 0 && args[0].Equals("--makeGifs")) {
-                // grab all countries, render and save them
-                CountryPoly earth = new CountryPoly();
-                // System.IO.StreamWriter sw = System.IO.File.AppendText("c:\\projects\\pcap\\country\\countries.txt");
-                System.IO.StreamWriter sw = System.IO.File.CreateText(installDir + "\\countryGif\\countries.txt");
-                foreach (string file in Util.GetFiles(installDir + "\\countryPoly", "*.txt")) {
-                    if (new FileInfo(file).Length == 0) {
-                        Console.WriteLine("Skipping " + file);
-                    } else {
-                        String txtFile = file.Substring(file.LastIndexOf("\\") + 1);
-                        String gifFile = "C:\\projects\\packetmap\\PacketMap\\countryGif\\" + txtFile.Substring(0, txtFile.IndexOf(".")) + ".png";
-                        Console.WriteLine("Creating " + gifFile + "...");
-                        CountryPoly country = new CountryPoly(file);
-                        country.saveToFile(gifFile, Color.LightGreen, Color.Transparent);
-                        earth.loadData(file);
-                        sw.WriteLine("{0} {1} {2} {3} {4}", txtFile.Substring(0, txtFile.IndexOf(".")),
-                            country.getMinLngLat().getLng(), country.getMinLngLat().getLat(),
-                            country.getMaxLngLat().getLng(), country.getMaxLngLat().getLat());
+            for (int i=0; i<args.Length; i++) {
+                string arg = args[i];
+                Console.WriteLine("Processing arg '" + arg + "'");
+                if (arg.Equals("--makeGifs")) {
+                    // grab all countries, render and save them
+                    CountryPoly earth = new CountryPoly();
+                    // System.IO.StreamWriter sw = System.IO.File.AppendText("c:\\projects\\pcap\\country\\countries.txt");
+                    System.IO.StreamWriter sw = System.IO.File.CreateText(installDir + "\\countryGif\\countries.txt");
+                    foreach (string file in Util.GetFiles(installDir + "\\countryPoly", "*.txt")) {
+                        if (new FileInfo(file).Length == 0) {
+                            Console.WriteLine("Skipping " + file);
+                        } else {
+                            String txtFile = file.Substring(file.LastIndexOf("\\") + 1);
+                            String gifFile = "C:\\projects\\packetmap\\PacketMap\\countryGif\\" + txtFile.Substring(0, txtFile.IndexOf(".")) + ".png";
+                            Console.WriteLine("Creating " + gifFile + "...");
+                            CountryPoly country = new CountryPoly(file);
+                            country.saveToFile(gifFile, Color.LightGreen, Color.Transparent);
+                            earth.loadData(file);
+                            sw.WriteLine("{0} {1} {2} {3} {4}", txtFile.Substring(0, txtFile.IndexOf(".")),
+                                country.getMinLngLat().getLng(), country.getMinLngLat().getLat(),
+                                country.getMaxLngLat().getLng(), country.getMaxLngLat().getLat());
+                        }
                     }
+                    earth.saveToFile(installDir + "\\countryGif\\earth.png", Color.FromArgb(62, 94, 67), Color.FromArgb(0, 5, 100));
+                    sw.WriteLine("earth {0} {1} {2} {3}",
+                        earth.getMinLngLat().getLng(), earth.getMinLngLat().getLat(),
+                        earth.getMaxLngLat().getLng(), earth.getMaxLngLat().getLat());
+                    sw.Close();
                 }
-                earth.saveToFile(installDir + "\\countryGif\\earth.png", Color.FromArgb(62, 94, 67), Color.FromArgb(0, 5, 100));
-                sw.WriteLine("earth {0} {1} {2} {3}",
-                    earth.getMinLngLat().getLng(), earth.getMinLngLat().getLat(),
-                    earth.getMaxLngLat().getLng(), earth.getMaxLngLat().getLat());
-                sw.Close();
+
+                if (arg.Equals("--makeFlagGif")) {
+                    // make flag composite image
+                    installDir = "c:\\projects\\packetmap\\PacketMap";
+                    int flagCount = 0;
+                    int maxW = 0, maxH = 0;
+                    foreach (string file in Util.GetFiles(installDir + "\\flags", "*.gif")) {
+                        Image miniFlag = Image.FromFile(file);
+                        maxW = Math.Max(maxW, miniFlag.Width);
+                        maxH = Math.Max(maxH, miniFlag.Height);
+                        flagCount++;
+                    }
+                    Console.WriteLine("FlagCount = " + flagCount);
+                    // each flag is 20px by 12px
+                    int flagsPerRow = 20;
+                    Bitmap b = new Bitmap(maxW * flagsPerRow, maxH * (flagCount / flagsPerRow) + maxH, PixelFormat.Format24bppRgb);
+                    Graphics g = Graphics.FromImage(b);
+                    g.FillRectangle(Brushes.White, 0, 0, maxW * flagsPerRow, maxH * (flagCount / flagsPerRow) + maxH);
+                    flagCount = 0;
+                    foreach (string file in Util.GetFiles(installDir + "\\flags", "*.gif")) {
+                        flagCount++;
+                        Image miniFlag = Image.FromFile(file);
+                        g.DrawImage(miniFlag, (flagCount % flagsPerRow) * maxW, (flagCount / flagsPerRow) * maxH);
+                    }
+                    g.Dispose();
+                    b.Save(installDir + "\\data\\flagComposite.png", ImageFormat.Png);
+                }
             }
 
 
